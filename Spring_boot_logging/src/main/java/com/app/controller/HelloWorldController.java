a50e2a262eae8c4caba6bdaef7c8378838263b1c
janar <janar@LAPTOP-4LA3M04D>
2020-01-24 22:23:33 +0530

    commit first

---
+++ Spring_boot_logging/src/main/java/com/app/controller/HelloWorldController.java
@@ -0,0 +1,31 @@
+package com.app.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(HelloWorldController.class);
+	
+	@GetMapping("/helloWorld/logging")
+	public void helloWorldProp() {
+		LOG.info("******* Info log is enabled *****");
+		LOG.debug("******* Debug log is enabled *****");
+		LOG.warn("******* Warn log is enabled *****");
+		LOG.error("******* Error log is enabled *****");
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
